535209b7f8459ea511e07fe6c88189e942b83090
xjhyyds <1289264578@qq.com>
2024-02-01 20:27:56 +0100

    ZR

--- run_no_grooming.py
+++ run_no_grooming.py
@@ -1,22 +1,18 @@
 import networkx as nx
 from utils.network import network as N
 import random
+from utils.utils import *
+import matplotlib.pyplot as plt
+import heapq
 
 
 
-def gen_request(num):
-    rate_list = [100, 200, 300, 400]
-    request = []
-    for i in range(0, num):
-        src, dst = random.sample(range(1, 8), 2)
-        rate = random.randint(0, 3)
 
-        request.append((src, dst, rate_list[rate]))
-
-    return request
-
-def ZR_serve(G, requests):
-    pass
+def ZR_serve(network, requests):
+    request_table = []
+    for i in requests:
+        shortest_path = find_shortest_path(i, network)
+        print(shortest_path)
 
 
 if __name__ == '__main__':
@@ -27,9 +23,8 @@ if __name__ == '__main__':
     ZR_OEO.get_topology()
     OEO.get_topology()
 
-    init_num_request = 350
+    init_num_request = 1
     request_list = gen_request(init_num_request)
+    request_list = [(2,3,400,1)]
 
     ZR_serve(ZR, request_list)
-
-
---
+++ utils/utils.py
@@ -0,0 +1,91 @@
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
+import heapq
+
+ZR_REACH_TABLE = {"16QAM": {"rate": 400, "channel": 75, "reach": 600},
+                  "8QAM": {"rate": 300, "channel": 75, "reach": 1800},
+                  "QPSK_1": {"rate": 200, "channel": 75, "reach": 3000},
+                  "QPSK_2": {"rate": 100, "channel": 50, "reach": 3000}}
+
+EOE_REACH_TABLE = {"PCS64QAM_1": {"rate": 800, "channel": 100, "reach": 150},
+                   "PCS64QAM_2": {"rate": 700, "channel": 100, "reach": 400},
+                   "16QAM_1": {"rate": 600, "channel": 100, "reach": 700},
+                   "PCS16QAM_1": {"rate": 500, "channel": 100, "reach": 1300},
+                   "PCS16QAM_2": {"rate": 400, "channel": 100, "reach": 2500},
+                   "PCS16QAM_3": {"rate": 300, "channel": 100, "reach": 4700},
+                   "64QAM": {"rate": 300, "channel": 50, "reach": 100},
+                   "16QAM_2": {"rate": 200, "channel": 50, "reach": 900},
+                   "QPSK": {"rate": 100, "channel": 50, "reach": 3000}}
+
+
+def gen_request(num):
+    rate_list = [100, 200, 300, 400]
+    request = []
+    for i in range(0, num):
+        src, dst = random.sample(range(1, 8), 2)
+        rate = random.randint(0, 3)
+
+        request.append((src, dst, rate_list[rate], i + 1))
+
+    return request
+
+
+def generate_resource_graph(network):
+    G = nx.Graph()
+    for u, v, data in network.topology.edges(data=True):
+        if data['occupied_channel'] == 96:
+            continue
+        else:
+            G.add_edge(u, v, **data)
+    return G
+
+
+def link_is_available(modulation, edge_data):
+    for num, mod in enumerate(modulation):
+        if edge_data['distance'] <= modulation[mod]['reach'] and edge_data['channels'] >= 1 + modulation[mod][
+            'channel'] / 2:
+            return True
+
+    return False
+
+
+def find_shortest_path(request, network):
+    # request[source, destination, rate, id]
+    requests = {"source": request[0], "destination": request[1], "rate": request[2], "id": request[3]}
+    modulation = {}
+    for num, mod in enumerate(ZR_REACH_TABLE):
+        if ZR_REACH_TABLE[mod]['rate'] == requests['rate']:
+            modulation[mod] = ZR_REACH_TABLE[mod]
+    G = generate_resource_graph(network)
+    start = requests["source"]
+    end = requests["destination"]
+    # modified dijkstra
+    distances = {node: float('infinity') for node in G.nodes}
+    previous_nodes = {node: None for node in G.nodes}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        if current_node == end:
+            break
+
+        for neighbor in G.neighbors(current_node):
+            edge_data = G.get_edge_data(current_node, neighbor)
+            tentative_distance = current_distance + edge_data['distance']
+            if link_is_available(modulation, edge_data):
+                if tentative_distance < distances[neighbor]:
+                    distances[neighbor] = tentative_distance
+                    previous_nodes[neighbor] = current_node
+                    heapq.heappush(priority_queue, (tentative_distance, neighbor))
+
+    path = []
+    current_node = end
+    while previous_nodes[current_node] is not None:
+        path.insert(0, current_node)
+        current_node = previous_nodes[current_node]
+    if path:
+        path.insert(0, current_node)
+    return path
